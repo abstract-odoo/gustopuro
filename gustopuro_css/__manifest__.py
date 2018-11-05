bd60e56f5ec29e569f811416dc7e5164feef6519
Francesco <Cesc@MacBook-Pro-di-Francesco.local>
2018-11-05 19:56:01 +0100

    Add Custom Css Module

---
+++ gustopuro_css/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Abstract Technology
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Gustopuro CSS modulo',
+    'description': """
+
+    """,
+    'version': '10.0.1.1.0',
+    'license': 'AGPL-3',
+    #'category': 'inventory',
+    'author': 'Abstract Technology',
+    'website': 'www.abstract.it',
+    'depends':[
+        'base',
+        'website',
+    ],
+    'data':[
+        'views/gustopuro_custom_css.xml',
+        ],
+    'installable': True,
+    'auto_install': False,
+}
